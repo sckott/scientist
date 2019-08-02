a53fa4eea39890788aa5dc95638de1fd6318d061
Scott Chamberlain <myrmecocystus@gmail.com>
2019-08-01 21:06:46 -0700

    import lazyeval

--- R/scientist-package.R
+++ R/scientist-package.R
@@ -2,6 +2,7 @@
 #'
 #' @importFrom R6 R6Class
 #' @importFrom callr r
+#' @importFrom lazyeval lazy_dots lazy_eval
 #' @author Scott Chamberlain \email{sckott@@protonmail.com}
 #' @docType package
 #' @aliases scientist-package
